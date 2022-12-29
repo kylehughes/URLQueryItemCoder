91a064a05e2a14f340f78e3d55e57afd886c4dba
Kyle Hughes <me@kylehugh.es>
2022-12-29 13:49:42 -0800

    Pass through line and file values to XCTAssert

--- Tests/URLQueryItemCodableTests/URLQueryItemEncoderTests.swift
+++ Tests/URLQueryItemCodableTests/URLQueryItemEncoderTests.swift
@@ -84,11 +84,13 @@ extension URLQueryItemEncoderTests {
     private func assert<Encoder, Input>(
         _ encoder: Encoder,
         encodes value: Input,
-        as expectation: (Input) -> Encoder.Output
+        as expectation: (Input) -> Encoder.Output,
+        file: StaticString = #filePath,
+        line: UInt = #line
     ) throws where Encoder: TopLevelEncoder, Encoder.Output: Equatable, Input: Encodable {
         let encodedValue = try encoder.encode(value)
         
-        XCTAssertEqual(encodedValue, expectation(value))
+        XCTAssertEqual(encodedValue, expectation(value), file: file, line: line)
     }
 }
 
