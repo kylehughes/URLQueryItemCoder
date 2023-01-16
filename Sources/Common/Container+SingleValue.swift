75e369b6d5fe0c2a0821f7bae3bff7cfdd7691e6
Kyle Hughes <me@kylehugh.es>
2023-01-15 18:56:22 -0800

    Implement the majority of decoding functions for Container.SingleValue… unsure if it will work

--- Sources/Common/Container+SingleValue.swift
+++ Sources/Common/Container+SingleValue.swift
@@ -21,10 +21,255 @@ extension Container {
     }
 }
 
+// MARK: - SingleValueDecodingContainer Extension
+
+extension Container.SingleValue: SingleValueDecodingContainer {
+    // MARK: Public Instance Interface
+    
+    public func decode(_ type: Bool.Type) throws -> Bool {
+        switch storage {
+        case let .primitive(primitive):
+            switch primitive {
+            case let .bool(value):
+                return value
+            default:
+                throw DecodingError.typeMismatch(type, .obvious(codingPath))
+            }
+        case .container:
+            throw DecodingError.typeMismatch(type, .obvious(codingPath))
+        case .none:
+            throw DecodingError.valueNotFound(type, .obvious(codingPath))
+        }
+    }
+    
+    public func decode(_ type: Double.Type) throws -> Double {
+        switch storage {
+        case let .primitive(primitive):
+            switch primitive {
+            case let .double(value):
+                return value
+            default:
+                throw DecodingError.typeMismatch(type, .obvious(codingPath))
+            }
+        case .container:
+            throw DecodingError.typeMismatch(type, .obvious(codingPath))
+        case .none:
+            throw DecodingError.valueNotFound(type, .obvious(codingPath))
+        }
+    }
+    
+    public func decode(_ type: Float.Type) throws -> Float {
+        switch storage {
+        case let .primitive(primitive):
+            switch primitive {
+            case let .float(value):
+                return value
+            default:
+                throw DecodingError.typeMismatch(type, .obvious(codingPath))
+            }
+        case .container:
+            throw DecodingError.typeMismatch(type, .obvious(codingPath))
+        case .none:
+            throw DecodingError.valueNotFound(type, .obvious(codingPath))
+        }
+    }
+    
+    public func decode(_ type: Int.Type) throws -> Int {
+        switch storage {
+        case let .primitive(primitive):
+            switch primitive {
+            case let .int(value):
+                return value
+            default:
+                throw DecodingError.typeMismatch(type, .obvious(codingPath))
+            }
+        case .container:
+            throw DecodingError.typeMismatch(type, .obvious(codingPath))
+        case .none:
+            throw DecodingError.valueNotFound(type, .obvious(codingPath))
+        }
+    }
+    
+    public func decode(_ type: Int8.Type) throws -> Int8 {
+        switch storage {
+        case let .primitive(primitive):
+            switch primitive {
+            case let .int8(value):
+                return value
+            default:
+                throw DecodingError.typeMismatch(type, .obvious(codingPath))
+            }
+        case .container:
+            throw DecodingError.typeMismatch(type, .obvious(codingPath))
+        case .none:
+            throw DecodingError.valueNotFound(type, .obvious(codingPath))
+        }
+    }
+    
+    public func decode(_ type: Int16.Type) throws -> Int16 {
+        switch storage {
+        case let .primitive(primitive):
+            switch primitive {
+            case let .int16(value):
+                return value
+            default:
+                throw DecodingError.typeMismatch(type, .obvious(codingPath))
+            }
+        case .container:
+            throw DecodingError.typeMismatch(type, .obvious(codingPath))
+        case .none:
+            throw DecodingError.valueNotFound(type, .obvious(codingPath))
+        }
+    }
+    
+    public func decode(_ type: Int32.Type) throws -> Int32 {
+        switch storage {
+        case let .primitive(primitive):
+            switch primitive {
+            case let .int32(value):
+                return value
+            default:
+                throw DecodingError.typeMismatch(type, .obvious(codingPath))
+            }
+        case .container:
+            throw DecodingError.typeMismatch(type, .obvious(codingPath))
+        case .none:
+            throw DecodingError.valueNotFound(type, .obvious(codingPath))
+        }
+    }
+    
+    public func decode(_ type: Int64.Type) throws -> Int64 {
+        switch storage {
+        case let .primitive(primitive):
+            switch primitive {
+            case let .int64(value):
+                return value
+            default:
+                throw DecodingError.typeMismatch(type, .obvious(codingPath))
+            }
+        case .container:
+            throw DecodingError.typeMismatch(type, .obvious(codingPath))
+        case .none:
+            throw DecodingError.valueNotFound(type, .obvious(codingPath))
+        }
+    }
+    
+    public func decode(_ type: String.Type) throws -> String {
+        switch storage {
+        case let .primitive(primitive):
+            switch primitive {
+            case let .string(value):
+                return value
+            default:
+                throw DecodingError.typeMismatch(type, .obvious(codingPath))
+            }
+        case .container:
+            throw DecodingError.typeMismatch(type, .obvious(codingPath))
+        case .none:
+            throw DecodingError.valueNotFound(type, .obvious(codingPath))
+        }
+    }
+    
+    public func decode(_ type: UInt.Type) throws -> UInt {
+        switch storage {
+        case let .primitive(primitive):
+            switch primitive {
+            case let .uint(value):
+                return value
+            default:
+                throw DecodingError.typeMismatch(type, .obvious(codingPath))
+            }
+        case .container:
+            throw DecodingError.typeMismatch(type, .obvious(codingPath))
+        case .none:
+            throw DecodingError.valueNotFound(type, .obvious(codingPath))
+        }
+    }
+    
+    public func decode(_ type: UInt8.Type) throws -> UInt8 {
+        switch storage {
+        case let .primitive(primitive):
+            switch primitive {
+            case let .uint8(value):
+                return value
+            default:
+                throw DecodingError.typeMismatch(type, .obvious(codingPath))
+            }
+        case .container:
+            throw DecodingError.typeMismatch(type, .obvious(codingPath))
+        case .none:
+            throw DecodingError.valueNotFound(type, .obvious(codingPath))
+        }
+    }
+    
+    public func decode(_ type: UInt16.Type) throws -> UInt16 {
+        switch storage {
+        case let .primitive(primitive):
+            switch primitive {
+            case let .uint16(value):
+                return value
+            default:
+                throw DecodingError.typeMismatch(type, .obvious(codingPath))
+            }
+        case .container:
+            throw DecodingError.typeMismatch(type, .obvious(codingPath))
+        case .none:
+            throw DecodingError.valueNotFound(type, .obvious(codingPath))
+        }
+    }
+    
+    public func decode(_ type: UInt32.Type) throws -> UInt32 {
+        switch storage {
+        case let .primitive(primitive):
+            switch primitive {
+            case let .uint32(value):
+                return value
+            default:
+                throw DecodingError.typeMismatch(type, .obvious(codingPath))
+            }
+        case .container:
+            throw DecodingError.typeMismatch(type, .obvious(codingPath))
+        case .none:
+            throw DecodingError.valueNotFound(type, .obvious(codingPath))
+        }
+    }
+    
+    public func decode(_ type: UInt64.Type) throws -> UInt64 {
+        switch storage {
+        case let .primitive(primitive):
+            switch primitive {
+            case let .uint64(value):
+                return value
+            default:
+                throw DecodingError.typeMismatch(type, .obvious(codingPath))
+            }
+        case .container:
+            throw DecodingError.typeMismatch(type, .obvious(codingPath))
+        case .none:
+            throw DecodingError.valueNotFound(type, .obvious(codingPath))
+        }
+    }
+    
+    public func decode<Target>(_ type: Target.Type) throws -> Target where Target: Decodable {
+        let lowLevelDecoder = LowLevelDecoder(codingPath: codingPath)
+        
+        return try Target(from: lowLevelDecoder)
+    }
+    
+    public func decodeNil() -> Bool {
+        switch storage {
+        case .none:
+            return true
+        default:
+            return false
+        }
+    }
+}
+
 // MARK: - SingleValueEncodingContainer Extension
 
 extension Container.SingleValue: SingleValueEncodingContainer {
-    // MARK: public Instance Interface
+    // MARK: Public Instance Interface
     
     public func encode(_ value: Bool) throws {
         storage = .primitive(.bool(value))
@@ -104,5 +349,11 @@ extension Container.SingleValue {
     public enum Storage {
         case container(Container)
         case primitive(PrimitiveValue)
+    }
+}
+
+extension DecodingError.Context {
+    internal static func obvious(_ codingPath: [any CodingKey]) -> Self {
+        Self(codingPath: codingPath, debugDescription: String())
     }
 }
